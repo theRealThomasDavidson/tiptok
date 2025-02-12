42a9d2dc34bc8d312a4cb1990d3f596fc19204d8
theRealThomasDavidson <davidson.thomasJ@gmail.com>
2025-02-12 14:48:49 -0600

    Fix merge conflict in MainActivity.kt

--- android/app/src/main/kotlin/dev/thomasdavidson/trainup/MainActivity.kt
+++ android/app/src/main/kotlin/dev/thomasdavidson/trainup/MainActivity.kt
@@ -3,8 +3,4 @@ package dev.thomasdavidson.trainup
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-<<<<<<< HEAD
 }
-=======
-} 
->>>>>>> 8698398ab18c8ab14834f221bca1510d90a23aec
